2ada0922aa42912e01cabe7a0dc622ea416c4617
PurredCoffee <owo@foxgirl.codes>
2026-03-20 17:11:27 +0100

    remove twitch and add dependency to twurple plugin

--- helpers.js
+++ helpers.js
@@ -1,21 +1,36 @@
-const files = require('./files');
-const server = require('./server');
-const subscribable = require('./subscribable');
-const twitch = require('./twitch');
-const twitch_data = require('./twitch_data');
-const obs = require('./obs');
-const persistent = require('./persistent');
-const selfClearing = require('./selfClearing');
-const { prefixDefaultFile } = require('../internal/clawffeeInternals');
+const files = require('./files.js');
+const server = require('./server.js');
+const subscribable = require('./subscribable.js');
+const twitch = require('../twurple/twitch.js');
+const fs = require('fs');
+if(!fs.existsSync('config/internal/twitch_data.js')) {
+    fs.writeFileSync('config/internal/twitch_data.js', 'module.exports = {"IDs":{},"redeems":{}}');
+}
+const twitch_data = require('../../config/internal/twitch_data.js');
+const obs = require('./obs.js');
+const persistent = require('./persistent.js');
+const selfClearing = require('./selfClearing.js');
+const { prefixDefaultFile } = require('../internal/clawffeeInternals.js');
 const path = require('path');
+const internal = require('../internal/clawffeeInternals.js');
 
-prefixDefaultFile((fullpath) => `const { files, server, twitch, twitch_data, obs, persistent, selfClearing } = require('${path.posix.relative(path.dirname(fullpath), __filename)}');\n`);
+prefixDefaultFile((fullpath) => `const { files, server, twurple, twitch_data, obs, persistent, selfClearing } = require('${path.relative(path.dirname(fullpath), __filename).replaceAll('\\', '/')}');\n`);
 
+const warnfiles = new Set();
 module.exports = {
     files,
     server,
     subscribable,
-    twitch,
+    twurple: twitch,
+    twitch: new Proxy(twitch, {
+        get(target, p, receiver) {
+            const s = internal.getRunningScriptName();
+            if(!warnfiles.has(s))
+                console.warn('Please do not use helpers.twitch in this version as it will change in future versions, use helpers.twurple instead!');
+            warnfiles.add(s);
+            return Reflect.get(target, p, receiver);
+        }
+    }),
     obs,
     twitch_data,
     persistent,
